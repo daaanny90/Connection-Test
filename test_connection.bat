60fe4d49359493364854728b53122912c7afb651
Danny <danny.spina@hotmail.it>
2018-02-26 14:59:02 +0100

    changed style and added log function

--- test_connection.bat
+++ test_connection.bat
@@ -1,12 +1,24 @@
 @echo off
+setlocal enableextensions enabledelayedexpansion
+mode con: cols=82 lines=20
 TITLE CONNECTION TEST
-echo #####################
-echo ## CONNECTION TEST ##
-echo #####################
-echo.
+echo "                                                _   _                         "
+echo "                 ___ ___  _ __  _ __   ___  ___| |_(_) ___  _ __              "
+echo "                / __/ _ \| '_ \| '_ \ / _ \/ __| __| |/ _ \| '_ \             "
+echo "               | (_| (_) | | | | | | |  __/ (__| |_| | (_) | | | |            "
+echo "                \___\___/|_| |_|_| |_|\___|\___|\__|_|\___/|_| |_|            "
+echo "                                                                              "
+echo "                                  _            _                              "
+echo "                                 | |_ ___  ___| |_                            "
+echo "                                 | __/ _ \/ __| __|                           "
+echo "                                 | ||  __/\__ \ |_                            "
+echo "                                  \__\___||___/\__|                           "
+echo "                                                                              "
 echo. 
-echo This tool will check the connection continuosly, showing in real time your connection status, 
-echo until you decide to stop it with ctrl+C.
+echo      This test will check the connection continuosly will generate a log file,
+echo                   until you decide to stop it with ctrl+C.
+echo.
+echo.
 echo.
 :choice
 set /P c=Do you want to start the connection test[Y/N]?
@@ -14,35 +26,53 @@ if /I "%c%" EQU "Y" goto :test
 if /I "%c%" EQU "N" goto :exit
 
 :test
+echo.
 echo :) Starting test...
+echo Connection test started at %time% of the %date% >> C:\Users\spina\Desktop\connection_log_%date%.txt
 timeout /t 3 > NUL
 cls
-echo #######################
-echo ## CONNECTION STATUS ##
-echo #######################
-echo.
 
 :loop
-ping 8.8.8.8 -n 1 -w 5000 >NUL
+rem formatting time that will be printed and saved into the log file
+set hour=%time:~0,2%
+rem Remove leading space if single digit
+if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
+set min=%time:~3,2%
+rem Remove leading space
+if "%min:~0,1%" == " " set min=0%min:~1,1%
+rem Seconds
+set secs=%time:~6,2%
+rem Remove leading space
+if "%secs:~0,1%" == " " set secs=0%secs:~1,1%
+
+ping 8.8.8.8 -n 1  >NUL
 if errorlevel 1 (
-echo|set /p="[91m#[0m"
-timeout /t 1 > NUL
-echo|set /p="[91m#[0m"
-timeout /t 1 > NUL
-echo|set /p="[91m#[0m"
-timeout /t 1 > NUL
-echo|set /p=" [91m No connection![0m"
-echo [91m %time%[0m
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                 [91m ############################################### [0m
+echo                 [91m ##  CONNECTION STATUS: INTERRUPTED %hour%:%min%:%secs%  ## [0m
+echo                 [91m ############################################### [0m
+echo The connection was INTERRUPTED at %hour%:%min%:%secs% >> C:\Users\spina\Desktop\connection_log_%date%.txt
 )else (
-echo|set /p="[92m#[0m"
-timeout /t 1 > NUL
-echo|set /p="[92m#[0m"
-timeout /t 1 > NUL
-echo|set /p="[92m#[0m"
-timeout /t 1 > NUL
-echo|set /p=" [92m You are connected![0m"
-echo [92m %time%[0m
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                [92m ############################################### [0m
+echo                [92m ##        CONNECTION STATUS: OK %hour%:%min%:%secs%     ## [0m
+echo                [92m ############################################### [0m
+echo The connection was OK at %hour%:%min%:%secs% >> C:\Users\spina\Desktop\connection_log_%date%.txt
 )
+timeout /t 2 > NUL
+cls
 goto loop
 
 :exit
